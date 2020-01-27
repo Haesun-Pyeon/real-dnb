de15bf2fd1f30c50eca53ec16dc67b2578a7cb0f
EHWAGABI <twoflowersj@likelion.org>
2020-01-27 20:44:58 +0900

    storedetail.css

--- static/js/base.js
+++ static/js/base.js
@@ -1,19 +1,19 @@
 var path = document.location.pathname;
 
-if (path.includes('bookmap')) {
-    result = 'MAP'
-} else if (path.includes('themamap')) {
+if (path.includes('themamap')) {
     result = 'THEME MAP'
+} else if (path.includes('bookmap/store')) {
+    result = 'INFOMATION'
+} else if (path.includes('realmap')) {
+    result = 'MAP'
 } else if (path.includes('board')) {
-    result = 'CULTURE PROGRAMS'
+    result = 'CULTURE'
 } else if (path.includes('mypage')) {
     result = 'MY PAGE'
 } else if (path.includes('signup')) {
     result = '회원가입'
 } else if (path.includes('login')) {
     result = '로그인'
-} else if (path.includes('storedetail')) {
-    result = 'INFORMATION'
 } else if (path.includes('ranking')) {
     result = 'RANKING'
 }
