07247454ab740144e606e1a80e408794c642bee1
EHWAGABI <twoflowersj@likelion.org>
2020-02-11 15:40:34 +0900

    thema map css complete

--- bookmap/forms.py
+++ bookmap/forms.py
@@ -2,12 +2,16 @@ from django import forms
 from .models import Review, Thema
 
 class ReviewForm(forms.ModelForm):
-    content = forms.CharField(widget=forms.Textarea(attrs={'placeholder':' 후기를 입력해주세요.'}), label='')
+    content = forms.CharField(widget=forms.Textarea(attrs={'placeholder':'후기를 입력해주세요'}), label='')
     class Meta:
         model = Review
         fields = ['content']
 
 class AddThemaForm(forms.ModelForm):
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'제목을 입력하세요'}), label='')
+    description = forms.CharField(widget=forms.Textarea(attrs={'placeholder':'내용을 입력하세요'}), label='')
+    img = forms.ImageField(widget=forms.FileInput(attrs={'placeholder':''}), label='∙ 업로드할 사진 선택')
+    private = forms.BooleanField(widget=forms.CheckboxInput(attrs={'placeholder':''}), label='∙ 비공개 테마로 작성하시겠습니까?')
     class Meta:
         model = Thema
         fields = ['title', 'description', 'img', 'private']
