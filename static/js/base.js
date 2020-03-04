1ddef8210e8473a44c50b791df7a85ec738710f7
Haesun-Pyeon <sme1357@naver.com>
2020-03-05 00:53:51 +0900

    오타고치는김에 path약간정리

--- static/js/base.js
+++ static/js/base.js
@@ -3,21 +3,22 @@ var path = document.location.pathname;
 if (path.includes('thema')) {
     result = 'THEME MAP'
 } else if (path.includes('bookmap/store')) {
-    result = 'INFOMATION'
+    result = 'INFORMATION'
 } else if (path.includes('realmap')) {
     result = 'MAP'
 } else if (path.includes('board')) {
     result = 'DAILY NEWS'
 } else if (path.includes('mypage')) {
     result = 'MY PAGE'
-} else if (path.includes('signup' | 'tag')) {
+} else if (path.includes('signup')) {
     result = '회원가입'
 } else if (path.includes('login')) {
     result = '로그인'
 } else if (path.includes('ranking')) {
     result = 'RANKING'
-}
-else {
+} else if (path.includes('chat')) {
+    result = 'MESSAGE'
+} else {
     result = '동네북'
 }
 
