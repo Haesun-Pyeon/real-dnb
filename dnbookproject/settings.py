fe6c771ab28b7f63caf242a4790bdb1ddb1db0df
Haesun-Pyeon <sme1357@naver.com>
2020-03-17 18:03:18 +0900

    주소

--- dnbookproject/settings.py
+++ dnbookproject/settings.py
@@ -172,7 +172,7 @@ CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
         'CONFIG': {
-            "hosts": [('127.0.0.1', 6379)],
+            "hosts": [('https://dnbook.live', 6379)],
         },
     },
 }
