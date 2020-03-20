1fb81a6522cc5185f60382dda2e92d61e40e08ee
Haesun-Pyeon <sme1357@naver.com>
2020-03-20 19:22:48 +0900

    board정리

--- culture/urls.py
+++ culture/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     path('board/', views.board, name='board'),
-    path('board2/', views.board2, name='board2'),
-
 ]
--- culture/views.py
+++ culture/views.py
@@ -1,39 +1,12 @@
 from django.shortcuts import render
 from bookmap.models import BookStore
-from el_pagination.decorators import page_template
 from django.core.paginator import Paginator
-import simplejson
-import random
+from django.db.models import Q
 # Create your views here.
 
-@page_template('list_page.html')
-def board(request, 
-    template='board.html',
-    extra_context=None):
-    bookstores = BookStore.objects.all()
-    insta = []
-    for a in bookstores:
-        if (a.insta != 'nan') and (a.insta not in insta):
-            insta.append(a.insta)
-        else: pass
-    random.shuffle(insta)
-    context = {
-        'insta':insta,
-    }
-    if extra_context is not None:
-        context.update(extra_context)
-    return render(request, template, context)
-
-def board2(request):
-    bookstores = BookStore.objects.all()
-    insta = []
-    for a in bookstores:
-        if (a.insta != 'nan') and (a.insta not in insta):
-            insta.append(a.insta)
-        else: pass
-    random.shuffle(insta)
-    insta_book = BookStore.objects.filter(insta__in =insta).order_by('?')
+def board(request):
+    insta_book = BookStore.objects.filter(~Q(insta='nan')).order_by('?')
     paginator = Paginator(insta_book,10)
     page = request.GET.get('page')
     pageposts=paginator.get_page(page)
-    return render(request, 'board2.html', {'pageposts':pageposts,})
+    return render(request, 'board.html', {'pageposts':pageposts,})
