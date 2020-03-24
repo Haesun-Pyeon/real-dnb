795dbbf2d056b69bb038ddcd57d11707076b31a1
Haesun-Pyeon <sme1357@naver.com>
2020-03-24 18:29:07 +0900

    호버호버

--- bookmap/forms.py
+++ bookmap/forms.py
@@ -12,7 +12,7 @@ class AddThemaForm(forms.ModelForm):
     title = forms.CharField(widget=forms.TextInput(attrs={'placeholder':'제목을 입력하세요'}), label='')
     description = forms.CharField(widget=forms.Textarea(attrs={'placeholder':'내용을 입력하세요'}), label='')
     img = forms.ImageField(widget=forms.FileInput(attrs={'placeholder': ''}), label='∙ 업로드할 사진 선택', allow_empty_file=True, required=False)
-    private = forms.NullBooleanField(widget=forms.CheckboxInput(attrs={'placeholder':''}), label='∙ 비공개 테마로 작성하시겠습니까?', required=False)
+    private = forms.NullBooleanField(widget=forms.CheckboxInput(attrs={'placeholder':'', 'id':'priv'}), label='∙ 비공개 테마로 작성하시겠습니까?', required=False)
     class Meta:
         model = Thema
         fields = ['title', 'description', 'img', 'private']
